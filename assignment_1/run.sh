dbdccdcaa6d98c321b5c8a35c61ee0ee5dd059ff
trinerye <103111583+trinerye@users.noreply.github.com>
2024-03-30 18:31:04 +0100

    Update run.sh

--- assignment_1/run.sh
+++ assignment_1/run.sh
@@ -1,6 +1,6 @@
 # activate env
 source ./A1_env/bin/activate
 # run the code
-python src/image_search_algorithm.py --image "image_1357.jpg" -p
+python src/image_search_algorithm.py --image "image_1357.jpg" --print_results
 # close the enviroments
 deactivate
