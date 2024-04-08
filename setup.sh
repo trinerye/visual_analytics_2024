f4adea22e9620545f31ffd3a4b633a2f323db878
trinerye <trine.rye@hotmail.com>
2024-04-08 14:16:28 +0200

    update setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,11 @@
+## Create virtual environment
+python -m venv main_env
+## Activate the environment
+source ./main_env/bin/activate
+## Install requirements
 sudo apt-get update
-
 sudo apt-get install -y python3-opencv
-
-pip install opencv-python matplotlib pandas numpy scikit-learn seaborn tensorflow
-
+pip install --upgrade pip
+pip install -r requirements.txt
+## Deactivate the environment
+deactivate 
